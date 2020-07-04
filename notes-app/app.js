6cba5a10068bc57c192239691f3321f03491d2cb
PhantomCracker <popa.vlad94@gmail.com>
2020-07-04 15:43:53 +0300

    Work in progress on Note App

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,67 @@
-const validator = require('validator');
-const fs = require('fs');
-const notes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-fs.writeFileSync('notes.txt', 'I have started the node Course.\n');
-fs.appendFileSync('notes.txt', 'And this is an append.');
+const notes = require('./notes');
 
-let myNotes = notes();
-console.log(myNotes);
+// const command = process.argv[2];
+// if(command === 'add') {
+//     console.log("Adding note!");
+// }
+// else if(command === 'remove') {
+//     console.log("Removing note!");
+// }
+// else {
+//     console.log("node app.js add/remove");
+// }
 
-let greenMessage = chalk.green("Success");
-console.log(greenMessage);
+// Create add command
+yargs.command({
+    command: 'add',
+    description: 'Add a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note content',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log("Title: " + argv.title);
+        console.log("Content: " + argv.body);
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    description: 'Remove a note',
+    handler: function () {
+        console.log("Removing the note");
+    }
+});
+
+// Create edit command
+yargs.command({
+    command: 'edit',
+    description: 'Edit a note',
+    handler: function () {
+        console.log("Editing a note");
+    }
+});
+
+// Creating list command
+yargs.command({
+    command: 'list',
+    description: 'Listing all the notes',
+    handler: function () {
+        console.log("All the notes are: ");
+    }
+})
+
+// console.log(yargs.argv);
+yargs.parse();
