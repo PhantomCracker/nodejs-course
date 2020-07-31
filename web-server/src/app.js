6ec2226d3cc6eed6d0a425fadb01bcbcca78bc54
PhantomCracker <popa.vlad94@gmail.com>
2020-07-31 18:45:07 +0300

    Creating views and partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,36 @@
 const path = require('path');
 const express = require('express');
+const hbs  = require('hbs');
 
- const app = express();
+const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const partialsPath = path.join(__dirname, '/views/partials');
 
-app.use(express.static(path.join(publicDirectoryPath)));
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
+hbs.registerPartials(partialsPath);
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'PhantomCracker',
+        defaultLayout: null
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'PhantomCracker'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help",
+        name: "PhantomCracker"
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
