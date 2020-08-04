ec7db68f423115d615b61f9260694ac8f29992eb
PhantomCracker <popa.vlad94@gmail.com>
2020-08-04 17:20:00 +0300

    Adding the location from input form to string query and returning the result

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,19 @@
 console.log("Client side javascript");
 
-fetch('http://localhost:3000/weather?address=bucharest').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = searchElement.value;
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        });
     });
 });
